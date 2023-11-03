cb3a5da58e22c32d57a4c16f74414a5295274195
Fiona Monfrinotti <fionamonf@gmail.com>
2023-11-02 23:22:30 -0300

    casi

--- main.js
+++ main.js
@@ -18,10 +18,35 @@ class Cuadrado {
     this.nombre = "Cuadrado";
     this.descripcionDelCalculo = "Lado x Lado";
     this.lado = lado;
-    this.area = Math.pow(this.lado,2);
+    this.area = Math.pow(this.lado, 2);
   }
   describir() {
     return `El ${this.nombre} de lado ${this.lado}, tiene un area de ${this.area} y se calcula asi: ${this.descripcionDelCalculo}`;
+  }
+}
+
+class Circulo {
+  constructor(radio) {
+    this.nombre = "Circulo";
+    this.descripcionDelCalculo = "π r²";
+    this.radio = radio;
+    this.area = Math.PI * Math.pow(this.radio, 2);
+  }
+  describir() {
+    return `El ${this.nombre} de radio ${this.radio}, tiene un area de ${this.area} y se calcula asi: ${this.descripcionDelCalculo}`;
+  }
+}
+
+class Rectangulo {
+  constructor(base, altura) {
+    this.nombre = "Rectangulo";
+    this.descripcionDelCalculo = "Base x Altura";
+    this.base = base;
+    this.altura = altura;
+    this.area = this.base * this.altura;
+  }
+  describir() {
+    return `El ${this.nombre} de base ${this.base} y altura ${this.altura}, tiene un area de ${this.area} y se calcula asi: ${this.descripcionDelCalculo}`;
   }
 }
 
@@ -33,11 +58,33 @@ const crearTriangulo = (e) => {
   mostrarResultados(figuras);
 };
 
+const crearCuadrado = (e) => {
+  const lado = document.getElementById("lado");
+  const nuevoCuadrado = new Cuadrado(lado.value);
+  figuras.push(nuevoCuadrado);
+  mostrarResultados(figuras);
+};
+
+const crearCirculo = (e) => {
+  const radio = document.getElementById("radio");
+  const nuevoCirculo = new Circulo(radio.value);
+  figuras.push(nuevoCirculo);
+  mostrarResultados(figuras);
+};
+
+const crearRectangulo = (e) => {
+  const base = document.getElementById("baseR");
+  const altura = document.getElementById("alturaR");
+  const nuevoRectangulo = new Rectangulo(base.value, altura.value);
+  figuras.push(nuevoRectangulo);
+  mostrarResultados(figuras);
+};
+
 const mostrarResultados = (figuras) => {
   const ul = document.getElementById("resultados");
   //   remuevo todos los li del ul
   ul.innerHTML = "";
- 
+
   //   Agrego un li por cada resultado
   for (const figura of figuras) {
     const nuevoLi = document.createElement("li");
